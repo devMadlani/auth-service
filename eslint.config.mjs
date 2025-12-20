0f304f4431cf0e06a48300acbbd4e586a9cf6017
Dev Madlani <madlanidev@gmail.com>
2025-12-20 16:15:00 +0530

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        ignores: [
+            'dist',
+            'node_modues',
+            'eslint.config.mjs',
+            'jest.config.ts',
+            '**/*.spec.ts',
+            '**/*.test.ts',
+        ],
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        rules: {
+            // 'no-console': 'error',
+            // 'dot-notation': 'error',
+        },
+    },
+)
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+'use strict'
+
+import app from './app'
+import { Config } from './config/index'
+import logger from './config/logger'
+
+const startServer = () => {
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT, () => {
+            logger.error('error')
+            logger.warn('hello')
+            logger.info('Listing on port', { port: PORT })
+        })
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+startServer()
