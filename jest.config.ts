deaaf1262ea5eaf75e2993ce036b646c7f0bf8e8
Dev Madlani <madlanidev@gmail.com>
2026-01-19 22:11:02 +0530

    change jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,29 @@
-import type { Config } from 'jest'
-import { createDefaultPreset } from 'ts-jest'
-
-const tsJestPreset = createDefaultPreset()
-
-const config: Config = {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
     testEnvironment: 'node',
+
+    // 👉 NEW WAY (not globals)
     transform: {
-        ...tsJestPreset.transform,
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: 'tsconfig.json',
+                useESM: false,
+            },
+        ],
     },
+
+    // 👉 THIS FIXES YOUR MODULE NOT FOUND
+    moduleDirectories: ['node_modules', 'src'],
+
     verbose: true,
     collectCoverage: true,
     coverageProvider: 'v8',
-    collectCoverageFrom: ['src/**/*.ts', '!tests/**', '!src/node_modules/**'],
-}
 
-export default config
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!tests/**',
+        '!**/node_modules/**',
+    ],
+}
