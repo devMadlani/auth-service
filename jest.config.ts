eed513b64ecef05d6185b0b67641954922c4d4a2
developerdesigner18 <developer.designer18@gmail.com>
2026-01-22 15:14:19 +0530

    change jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,20 +3,6 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
 
-    // 👉 NEW WAY (not globals)
-    transform: {
-        '^.+\\.tsx?$': [
-            'ts-jest',
-            {
-                tsconfig: 'tsconfig.json',
-                useESM: false,
-            },
-        ],
-    },
-
-    // 👉 THIS FIXES YOUR MODULE NOT FOUND
-    moduleDirectories: ['node_modules', 'src'],
-
     verbose: true,
     collectCoverage: true,
     coverageProvider: 'v8',
